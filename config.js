f135f0141fa5b2ac9a2d3a98d140b4ad88c1ec1f
novusjendral <yanceblog123@gmail.com>
2021-06-25 03:18:23 +0700

    add all new file

---
+++ config.js
@@ -0,0 +1,28 @@
+global.owner = ['6285782946968','62821250725974'] // Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '055aed861b8b72bf'
+}
+
+// Sticker WM
+global.packname = 'I hope you\'re fine'
+global.author = 'Arie'
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
